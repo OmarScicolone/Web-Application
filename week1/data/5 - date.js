b32580cb94878380741e35f6d4774778fd252c98
Omar Scicolone <omar.scicolone@gmail.com>
2022-03-03 11:42:38 +0100

    file data

---
+++ week1/data/5 - date.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const dayjs = require('dayjs');
+
+let now = dayjs();
+
+console.log(now.format());
+
+let libretto = [];
+
+let esame = {nome: "AW1", voto:30, data: dayjs('2022-03-03')};
+console.log(esame);
+
+function Exam(nome, voto, data){
+   this.nome = nome;
+   this.voto = voto;
+   this.data = data;
+   this.str = function(){return `${this.nome} ${this.voto} ${this.data.format()}`}
+}
+   const esame2 = new Exam('WA1', 31, dayjs('2022-12-12'));
+   console.log(esame2.str())
